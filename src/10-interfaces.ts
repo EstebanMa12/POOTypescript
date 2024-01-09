5cc86591c4bcdf56fc7f9db366492f5830d45095
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-09 16:47:51 -0500

    Implement the Driver interface in PostgresDriver and OracleDriver classes

---
+++ src/10-interfaces.ts
@@ -0,0 +1,48 @@
+// Interfaces en clases
+// Las interfaces en clases funcionan como contratos, nos permiten tipar tanto los atributos como los métodos.
+// Las interfaces en clases también pueden ser implementadas por otras clases.
+// Las interfaces en clases pueden heredar de otras interfaces.
+
+export interface Driver{
+    database: string;
+    password: string;
+    port: number;
+
+    connect(): void;
+    disconnect(): void;
+    isConnected(name: string):boolean;
+}
+
+class PostgresDriver implements Driver{
+  constructor(
+    public database: string,
+    public password: string,
+    public port: number
+  ){}
+  connect() {
+    console.log('Conectando a Postgres');
+  }
+  disconnect() {
+    console.log('Desconectando de Postgres');
+  }
+  isConnected(name: string):boolean {
+    return true;
+  }
+}
+
+class OracleDriver implements Driver{
+  constructor(
+    public database: string,
+    public password: string,
+    public port: number
+  ){}
+  connect() {
+    console.log('Conectando a Oracle');
+  }
+  disconnect() {
+    console.log('Desconectando de Oracle');
+  }
+  isConnected(name: string):boolean {
+    return true;
+  }
+}
