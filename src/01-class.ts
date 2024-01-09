8994e4b8f88aa4e649ff5c50f36b43a09a80ca47
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-09 15:12:35 -0500

    Update TypeScript target to esnext

---
+++ src/01-class.ts
@@ -0,0 +1,18 @@
+class myDate{
+    day: number;
+    month: number;
+    year: number;
+
+    constructor(day: number, month: number, year: number){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    
+}
+
+let date = new myDate(1, 1, 2020);
+console.log(date.day);
+console.log(date.month);
+console.log(date.year);
