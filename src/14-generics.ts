c0e9db912f489b5f9725c23318571d6ae4aaf267
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-10 18:52:02 -0500

    Added type safety for returned values in the getValue function.

--- src/14-generics.ts
+++ src/14-generics.ts
@@ -1,7 +1,9 @@
 // You can use generics in functions to infer types based on what you call your function with.
-
+// Function to get value of any type
 function getValue<myType>(value: myType){
-    return value;
+  // Return the value as is
+  return value;
+  // Type safety for any returned value
 }
 getValue(12).toPrecision();
 getValue("12").toUpperCase();
