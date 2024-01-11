b55fd07ed4380bb31358eb530591187eea989cb4
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-10 20:57:52 -0500

    Update category DTO validation rules

--- src/app/dtos/category.dto.ts
+++ src/app/dtos/category.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsUrl } from "class-validator";
+import { IsEnum, IsNotEmpty, IsOptional, IsUrl, Length, validateOrReject } from "class-validator";
 
 import { AccessType, Category } from "../models/category.models";
 
@@ -7,8 +7,26 @@ export interface ICreateCategoryDto extends Omit<Category, 'id'>{}
 
 
 export class CreateCategoryDto implements ICreateCategoryDto{
+
+  @IsNotEmpty()
+  @Length(4,140)
   name!:  string;
   @IsUrl()
+  @IsNotEmpty()
   image!: string;
+  @IsEnum(AccessType)
+  @IsOptional()
   access?: AccessType|undefined;
 }
+
+(async()=>{
+  try {
+    const dto = new CreateCategoryDto();
+    dto.name = 'h';
+    dto.image = 'https://google.com';
+    await validateOrReject(dto)
+    console.log(dto);
+  } catch (error) {
+    console.log(error);
+  }
+})()
